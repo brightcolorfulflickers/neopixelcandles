37cf04b186b631076d4191987c96cf5f91f4eae5
brightcolorfulflickers <pursuitofnerdiness@gmail.com>
2016-10-03 22:27:18 -0400

    Feather bluetooth candle

---
+++ code/feathercandle/flicker.h
@@ -0,0 +1,39 @@
+// Any unconnected pin, to try to generate a random seed
+#define UNCONNECTED_PIN         2
+
+// The LED can be in only one of these states at any given time
+#define BRIGHT                  0
+#define UP                      1
+#define DOWN                    2
+#define DIM                     3
+#define BRIGHT_HOLD             4
+#define DIM_HOLD                5
+
+// Percent chance the LED will suddenly fall to minimum brightness
+#define INDEX_BOTTOM_PERCENT    10
+// Absolute minimum red value (green value is a function of red's value)
+#define INDEX_BOTTOM            128
+// Minimum red value during "normal" flickering (not a dramatic change)
+#define INDEX_MIN               192
+// Maximum red value
+#define INDEX_MAX               255
+
+// Decreasing brightness will take place over a number of milliseconds in this range
+#define DOWN_MIN_MSECS          20
+#define DOWN_MAX_MSECS          250
+// Increasing brightness will take place over a number of milliseconds in this range
+#define UP_MIN_MSECS            20
+#define UP_MAX_MSECS            250
+// Percent chance the color will hold unchanged after brightening
+#define BRIGHT_HOLD_PERCENT     20
+// When holding after brightening, hold for a number of milliseconds in this range
+#define BRIGHT_HOLD_MIN_MSECS   0
+#define BRIGHT_HOLD_MAX_MSECS   100
+// Percent chance the color will hold unchanged after dimming
+#define DIM_HOLD_PERCENT        5
+// When holding after dimming, hold for a number of milliseconds in this range
+#define DIM_HOLD_MIN_MSECS      0
+#define DIM_HOLD_MAX_MSECS      50
+
+#define MINVAL(A,B)             (((A) < (B)) ? (A) : (B))
+#define MAXVAL(A,B)             (((A) > (B)) ? (A) : (B))
